f4aec6e94d3bdadb4066c40a7d6c25d5e20559ab
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-04-03 04:49:58 +0900

    Refactored a test method.

--- Domain.Tests/MeasurementAggregatorTestShould.cs
+++ Domain.Tests/MeasurementAggregatorTestShould.cs
@@ -3,21 +3,24 @@ using Xunit;
 
 namespace Domain.Tests
 {
-    public class MeasurementAggregatorTestShould : IClassFixture<Mother>
+    public class MeasurementAggregatorTestShould : IClassFixture<Mother>, IClassFixture<AveragingCalculator>
     {
         private readonly MeasurementAggregator aggregator;
+        private readonly IAveragingCalculator calculator;
 
-        public MeasurementAggregatorTestShould(Mother mother)
+        public MeasurementAggregatorTestShould(Mother mother, AveragingCalculator calculator)
         {
             aggregator = new MeasurementAggregator(mother.Get4Measurements());
+            this.calculator = calculator;
         }
 
         [Theory]
         [InlineData(4, 1)]
-        public void ProduceSingleResultWhenGroupingByFour(int groupSize, int expectedNumberOfGroups)
+        [InlineData(2, 2)]
+        public void ProduceExpectedNumberOfResultWhenGroupingByGroupSize(int groupSize, int expectedNumberOfResults)
         {
-            var result = aggregator.Aggregate(new SizeGrouper(groupSize), new AveragingCalculator());
-            Assert.Equal(expectedNumberOfGroups, result.Count());
+            var result = aggregator.Aggregate(new SizeGrouper(groupSize), calculator);
+            Assert.Equal(expectedNumberOfResults, result.Count());
         }
     }
 }
