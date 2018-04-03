f3336add8f4603052a81e51a985b381847a3661a
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-04-03 11:22:07 +0900

    Implemented some code for Demo: Asserting Yourself video clip.

--- Domain.Tests/SizeGrouperTestShould.cs
+++ Domain.Tests/SizeGrouperTestShould.cs
@@ -18,13 +18,34 @@ namespace Domain.Tests
         [InlineData(4, 2, 2)]
         public void ProduceGroupOfSizeIfListOfOneIsGroupedBy(int dataSize, int sizeOfEachGroup, int numberOfGroups)
         {
+            // Arrange
             var measurements = mother.CreateMeasurementListOfSize(dataSize);
 
+            // Act
             var grouper = new SizeGrouper(sizeOfEachGroup);
             var groupedResults = grouper.Group(measurements);
 
+            // Assert
             Assert.Equal(numberOfGroups, groupedResults.Count());
             Assert.True(groupedResults.All(g => g.Count() == sizeOfEachGroup));
+        }
+
+
+        [Theory]
+        [InlineData(1, 1, 1)]
+        [InlineData(2, 1, 2)]
+        [InlineData(4, 2, 2)]
+        public void NotProduceNullMeasurements(int dataSize, int sizeOfEachGroup, int numberOfGroups)
+        {
+            // Arrange
+            var measurements = mother.CreateMeasurementListOfSize(dataSize);
+            var grouper = new SizeGrouper(sizeOfEachGroup);
+
+            // Act
+            var groupedResults = grouper.Group(measurements);
+
+            // Assert
+            Assert.True(groupedResults.All(result => !result.Contains(null)));
         }
     }
 }
